57924a63d59d06c1435658fb2aaca57cb7e1c6d5
alanturing <632682292@qq.com>
2017-09-05 11:21:12 +0800

    Initial commit

---
+++ app/src/main/java/bodor/com/bottomsheet/MainActivity.java
@@ -0,0 +1,29 @@
+package bodor.com.bottomsheet;
+
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View bottomsheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomsheet);
+
+        Button button = (Button) findViewById(R.id.button_1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+    }
+}
